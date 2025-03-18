4a1b9f0ae5f5e272a85631f360af9792f209afed
Athul krishna K S <athulkrishna736@gmail.com>
2025-03-18 21:21:59 +0530

    random js workouts

---
+++ basic-JS/function.js
@@ -0,0 +1,143 @@
+//first class function
+const greet = function (name) {
+    return name
+}
+// console.log(greet('athul'))
+
+//hoc function
+function add(a, b, fn){
+    return fn(a,b)
+}
+
+function fn(val1, val2){
+    return val1 + val2
+}
+// console.log(add(1,3, fn))
+
+//pure function 
+function add1(a,b){
+    return a + b
+}
+
+// console.log(add1(1,2))
+
+//factory function 
+function createUser(name, age){
+    return {
+        name: name,
+        age: age,
+        greet(){
+            return `hi iam ${this.name}`
+        }
+    }
+}
+
+const user1 = createUser('athul', 25)
+// console.log(user1.greet())
+
+
+//iife function 
+// (function() {
+//     console.log("I am an IIFE!");
+// })();
+
+
+
+//callbackfunction 
+function hello(cb){
+    cb()
+}
+
+function cb(){
+    console.log('THIS IS am callbackfunction')
+}
+
+// hello(cb)
+
+
+//arrow function
+const arrow = ()=> {
+    console.log('simple arrow function')
+}
+
+
+//generator function 
+function* numGen(){
+    yield 1;
+    yield 2;
+    yield 3;
+}
+
+const gen = numGen()
+// console.log(gen.next().value)
+// console.log(gen.next().value)
+// console.log(gen.next().value)
+// console.log(gen.next().value)
+
+
+function add2 (x){
+    return function inner(y){
+        return x + y
+    }
+}
+
+let addition = add2(10)
+// console.log(addition(15))
+
+let animal = {
+    eats: true
+}
+
+let rabbit = {
+    jumps: true
+}
+
+rabbit.__proto__ = animal
+
+console.log(rabbit.eats)
+console.log(rabbit.jumps)
+
+
+function* numberGenerator() {
+    let number = 1;
+    while (number <= 3) {
+        yield number;
+        number++;
+    }
+}
+
+const generator = numberGenerator();
+
+console.log(generator.next()); // { value: 1, done: false }
+console.log(generator.next()); // { value: 2, done: false }
+console.log(generator.next()); // { value: 3, done: false }
+console.log(generator.next()); // { value: undefined, done: true }
+
+
+
+function add(a, b, c) {
+    return a + b + c;
+}
+
+
+
+function add(a) {
+    return function(b) {
+        return function(c) {
+            return a + b + c;
+        };
+    };
+}
+
+
+const myPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('Promise resolved!');
+    }, 1000);
+});
+
+myPromise.then((message) => {
+    console.log(message); // Outputs: Promise resolved!
+}).catch((error) => {
+    console.error(error);
+});
